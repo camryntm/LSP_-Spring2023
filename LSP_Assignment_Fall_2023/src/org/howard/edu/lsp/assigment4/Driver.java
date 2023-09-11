22e18d6ac9e2fc18f3bc7c0bc8d2342fc1fa2788
Camcraker03 <Camcraker03@localhost>
2023-09-10 21:58:02 -0400

    Assignment 4 without Javadocs working through roadblock "Javadocs not supported"

---
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assigment4/Driver.java
@@ -0,0 +1,137 @@
+package org.howard.edu.lsp.assigment4;
+
+import org.howard.edu.lsp.assigment4.IntegerSet.IntegerSetException;
+
+/**
+ * A driver class to test the IntegerSet class and its methods.
+ * 
+ * @author Your Name
+ */
+public class Driver {
+
+    /**
+     * The main method that runs tests on the IntegerSet class methods.
+     * 
+     * @param args The command line arguments (not used in this application).
+     */
+    public static void main(String[] args) {
+        try {
+            // Declaring and initializing our first set
+            IntegerSet set1 = new IntegerSet();
+            
+            // Adding integers to set1 with a for loop
+            for (int i = 1; i <= 10; i++) {
+                set1.add(i);
+            }
+            
+            // Testing toString(), smallest(), and largest()
+            System.out.println("Value of Set 1 is: " + set1.toString() + "\n");
+            System.out.println("Smallest value in Set 1 is: " + set1.smallest()); // Result of smallest on set1
+            System.out.println("Largest value in Set 1 is: " + set1.largest() + "\n"); // Result of largest on set1
+            
+            // Testing remove()
+            System.out.println("Removed 3 from Set 1.");
+            set1.remove(3);
+            System.out.println("New value of Set 1: " + set1.toString() + "\n"); // Result of removing 3 from set1
+            
+            System.out.println("Removed 8 from Set 1.");
+            set1.remove(8);
+            System.out.println("New value of Set 1: " + set1.toString() + "\n"); // Result of removing 8 from set1
+            
+            System.out.println("Remove 100 from Set 1.");
+            set1.remove(100);
+            System.out.println("New value of Set 1: " + set1.toString() + "\n"); // Result of removing 100 from set1
+            
+            // Testing isEmpty()
+            System.out.println("Set 1 is Empty (true/false)");
+            System.out.println(set1.isEmpty() + "\n"); // Result of isEmpty on set1
+            
+            // Testing contains()
+            System.out.println("Set 1 contains 9? (true/false)");
+            System.out.println(set1.contains(9) + "\n"); // Result of contains(9) on set1
+            
+            // Testing length()
+            System.out.println("Length of Set 1: " + set1.length() + "\n"); // Result of length() on set1
+            
+            // "Resetting" the set
+            set1.clear();
+            for (int i = 1; i <= 10; i++) {
+                set1.add(i);
+            }
+            
+            // Declaring and initializing our second set
+            IntegerSet set2 = new IntegerSet();
+            
+            // Adding integers to set2 with a for loop
+            for (int i = 0; i <= 20; i += 2) {
+                set2.add(i);
+            }
+            
+            System.out.println("Value of Set 1 is: " + set1.toString());
+            System.out.println("Value of Set 2 is: " + set2.toString());
+            System.out.println("We will reset the values of each list after every test." + "\n");
+            
+            // Testing union()
+            System.out.println("Union of Set 1 and Set 2");
+            set1.union(set2);	// Union of set1 and set2
+            System.out.println(set1.toString() + "\n");	// Result of union of set1 and set2
+            
+            set1.clear();
+            for (int i = 1; i <= 10; i++) {
+                set1.add(i);
+            }
+            set2.clear();
+            for (int i = 0; i <= 20; i += 2) {
+                set2.add(i);
+            }
+            
+            System.out.println("Difference of Set 1 and Set 2");
+            set1.diff(set2); // Difference of set1 and set2
+            System.out.println(set1.toString() + "\n"); // Result of difference of set1 and set2
+            
+            set1.clear();
+            for (int i = 1; i <= 10; i++) {
+                set1.add(i);
+            }
+            set2.clear();
+            for (int i = 0; i <= 20; i += 2) {
+                set2.add(i);
+            }
+            
+            System.out.println("Difference of Set 2 and Set 1");
+            set2.diff(set1); // Difference of set2 and set1
+            System.out.println(set2.toString() + "\n"); // Result of difference of set2 and set1
+            
+            set1.clear();
+            for (int i = 1; i <= 10; i++) {
+                set1.add(i);
+            }
+            set2.clear();
+            for (int i = 0; i <= 20; i += 2) {
+                set2.add(i);
+            }
+            
+            System.out.println("Intersection of Set 1 and Set 2");
+            set1.intersect(set2); // Intersection of set1 and set2
+            System.out.println(set1.toString() + "\n"); // Result of intersection of set1 and set2
+            
+            set1.clear();
+            for (int i = 1; i <= 10; i++) {
+                set1.add(i);
+            }
+            set2.clear();
+            for (int i = 0; i <= 20; i += 2) {
+                set2.add(i);
+            }
+            
+            System.out.println("Are Set 1 and Set 2 equal?");
+            System.out.println(set1.equals(set2) + "\n"); // Result of equal on set1 and set 2
+            
+            System.out.println("Catching the exception");
+            set1.clear(); // Need the list to be empty
+            set1.largest(); // Largest throws the exception when the set is empty
+        } catch (IntegerSetException e) {
+            System.out.println(e); // Prints the exception
+        }
+    }
+}
---
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assigment4/IntegerSet.java
@@ -0,0 +1,207 @@
+package org.howard.edu.lsp.assigment4;
+
+import java.util.*;
+
+/**
+ * A class that represents an integer set. This class allows you to perform various operations
+ * on sets of integers, such as union, intersection, addition, removal, and more.
+ * 
+ * @author Camryn Turner-Markes
+ */
+public class IntegerSet {
+
+    private List<Integer> int_set;
+
+    /**
+     * Constructs an empty IntegerSet.
+     */
+    public IntegerSet() {
+        int_set = new ArrayList<Integer>();
+    }
+
+    /**
+     * Clears the internal representation of the set, making it empty.
+     */
+    public void clear() {
+        int_set.clear();
+    }
+    
+    /**
+     * Sets the internal representation of the set to a new list.
+     * 
+     * @param new_set The new list to set as the internal representation of the set.
+     */
+    public void set_set(List<Integer> new_set) {
+        int_set = new_set;
+    }
+
+    /**
+     * Returns the number of elements in the set.
+     * 
+     * @return The number of elements in the set.
+     */
+    public int length() {
+        return int_set.size();
+    }
+
+    /**
+     * Checks if two IntegerSet objects are equal. Two sets are equal if they contain
+     * the same values in any order.
+     * 
+     * @param a Another IntegerSet to compare.
+     * @return true if the sets are equal, false otherwise.
+     */
+    public boolean equals(IntegerSet a) {
+        if (int_set.size() != a.length()) {
+            return false;
+        } else {
+            return int_set.containsAll(a.int_set);
+        }
+    }
+
+    /**
+     * Checks if the set contains a specific value.
+     * 
+     * @param value The integer value to check for.
+     * @return true if the value is in the set, false otherwise.
+     */
+    public boolean contains(int value) {
+        return int_set.contains(value);
+    }
+
+    /**
+     * Returns the largest integer in the set.
+     * 
+     * @return The largest integer in the set.
+     * @throws IntegerSetException if the set is empty.
+     */
+    public int largest() throws IntegerSetException {
+        if (int_set.isEmpty()) {
+            throw new IntegerSetException("The Set is Empty.");
+        } else {
+            int large = int_set.get(0);
+            for (int i = 0; i < int_set.size(); i++) {
+                if (int_set.get(i) > large) {
+                    large = int_set.get(i);
+                }
+            }
+            return large;
+        }
+    }
+
+    /**
+     * Returns the smallest integer in the set.
+     * 
+     * @return The smallest integer in the set.
+     * @throws IntegerSetException if the set is empty.
+     */
+    public int smallest() throws IntegerSetException {
+        if (int_set.isEmpty()) {
+            throw new IntegerSetException("The Set is Empty.");
+        } else {
+            int small = int_set.get(0);
+            for (int i = 0; i < int_set.size(); i++) {
+                if (int_set.get(i) < small) {
+                    small = int_set.get(i);
+                }
+            }
+            return small;
+        }
+    }
+
+    /**
+     * Adds an integer to the set if it is not already present.
+     * 
+     * @param item The integer to add to the set.
+     */
+    public void add(int item) {
+        if (!int_set.contains(item)) {
+            int_set.add(item);
+        }
+    }
+
+    /**
+     * Removes an integer from the set if it is present.
+     * 
+     * @param item The integer to remove from the set.
+     */
+    public void remove(int item) {
+        if (int_set.contains(item)) {
+            int_set.remove(Integer.valueOf(item));
+        }
+    }
+
+    /**
+     * Calculates the union of two sets.
+     * 
+     * @param b Another IntegerSet to perform the union with.
+     */
+    public void union(IntegerSet b) {
+        for (int i = 0; i < b.length(); i++) {
+            if (!int_set.contains(b.int_set.get(i))) {
+                int_set.add(b.int_set.get(i));
+            }
+        }
+    }
+
+    /**
+     * Calculates the intersection of two sets.
+     * 
+     * @param b Another IntegerSet to perform the intersection with.
+     */
+    public void intersect(IntegerSet b) {
+        int_set.retainAll(b.int_set);
+    }
+
+    /**
+     * Calculates the difference between two sets (s1 - s2).
+     * 
+     * @param b Another IntegerSet to calculate the difference with.
+     */
+    public void diff(IntegerSet b) {
+        for (int i = 0; i < b.length(); i++) {
+            if (int_set.contains(b.int_set.get(i))) {
+                int_set.remove(Integer.valueOf(b.int_set.get(i)));
+            }
+        }
+    }
+
+    /**
+     * Checks if the set is empty.
+     * 
+     * @return true if the set is empty, false otherwise.
+     */
+    public boolean isEmpty() {
+        return int_set.isEmpty();
+    }
+
+    /**
+     * Returns a string representation of the set.
+     * 
+     * @return A string containing the elements of the set.
+     */
+    public String toString() {
+        StringBuilder list = new StringBuilder();
+        for (int i = 0; i < int_set.size(); i++) {
+            list.append(int_set.get(i));
+            if (i < int_set.size() - 1) {
+                list.append(", ");
+            }
+        }
+        return list.toString();
+    }
+
+    /**
+     * A custom exception class for IntegerSet operations.
+     */
+    public class IntegerSetException extends Exception {
+        /**
+         * Constructs an IntegerSetException with a specified error message.
+         * 
+         * @param error The error message.
+         */
+        public IntegerSetException(String error) {
+            super(error);
+        }
+    }
+}
